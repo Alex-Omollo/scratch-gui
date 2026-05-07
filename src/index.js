4a359beed3a0a2f96e36001c9bbd6100de0130cf
Blackgover <71961064+Alex-Omollo@users.noreply.github.com>
2026-05-07 16:57:10 +0300

    add vercel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import { inject } from "@vercel/analytics";
-inject();
-
 import GUI from './containers/gui.jsx';
 import AppStateHOC from './lib/app-state-hoc.jsx';
 import GuiReducer, {guiInitialState, guiMiddleware, initEmbedded, initFullScreen, initPlayer} from './reducers/gui';
